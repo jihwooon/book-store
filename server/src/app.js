160fb4afb60b8fac2aac6acdd6783b9641f09bac
jihwooon <jihwooon@gmail.com>
2023-12-31 21:48:17 +0900

    logger에 요청 추적기 ID를 추가하라
    logger에 요청이 발생 시 requestId가 추가 되도록 구성했습니다.
    log 파일에는 JSON 형태로 저장하도록 winston 포맷을 Json으로 출력 되도록
    수정하고 Meta데이터에 rTracerID를 추가했습니다
    
    seeAlso:
    - https://github.com/puzpuzpuz/cls-rtracer/issues/38
    - https://github.com/puzpuzpuz/cls-rtracer

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import rTracer from 'cls-rtracer';
 
 const app = express();
+
 app.use(express.json());
+app.use(rTracer.expressMiddleware());
 
 app.get('/', async (req, res) => {
   res.send('Hello World');
