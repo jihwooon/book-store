9b104c51cd6f23613b01738e4b05b15a15d499a1
jihwooon <jihwooon@gmail.com>
2024-01-03 02:15:01 +0900

    회원 가입 유저 정보를 데이터베이스에 저장하라
    POST url 요청 시 회원 정보를 데이터베이스에 저장합니다.

--- server/src/users/controller/signup.controller.ts
+++ server/src/users/controller/signup.controller.ts
@@ -1,13 +1,26 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-const signupController = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+import logger from '../../config/logger';
+import { save } from '../domain/user.repository';
 
-  res.status(StatusCodes.CREATED).json({
-    email,
-    password,
-  });
+const childLogger = logger.child({
+  label: 'signup.controller.ts',
+});
+
+const signupController = async (req: Request, res: Response) => {
+  const { email, password, name } = req.body;
+
+  const registeredUser = await save(email, password, name);
+  if (!registeredUser) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      error: '중복된 항목이 있습니다.',
+    });
+  }
+
+  childLogger.info(registeredUser);
+
+  return res.status(StatusCodes.CREATED).json('회원 가입이 정상적으로 되었습니다.');
 };
 
 export default signupController;
---
+++ server/src/users/domain/user.repository.ts
@@ -0,0 +1,26 @@
+import doQuery from '../../mariadb';
+import logger from '../../config/logger';
+
+const childLogger = logger.child({
+  label: 'user.repository.ts',
+});
+
+export const save = async (
+  email: string,
+  password: string,
+  name: string,
+): Promise<boolean> => {
+  try {
+    await doQuery((connection) => connection.execute(
+      'INSERT INTO users (email, password, name) VALUES (?, ?, ?)',
+      [email, password, name],
+    ));
+    return true;
+  } catch (error: any) {
+    if (/Duplicate entry/.test(error.message)) {
+      childLogger.error(error.message);
+      return false;
+    }
+    throw error;
+  }
+};
