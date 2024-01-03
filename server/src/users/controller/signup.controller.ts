b93a0225c9163fb47affb79b8ed0d437ef3dccbb
jihwooon <jihwooon@gmail.com>
2024-01-04 00:35:47 +0900

    불필요한 요소를 제거하고 데이터베이스 환경변수를 설정하라

--- server/src/users/controller/signup.controller.ts
+++ server/src/users/controller/signup.controller.ts
@@ -1,12 +1,7 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-import logger from '../../config/logger';
 import { save } from '../domain/user.repository';
-
-const childLogger = logger.child({
-  label: 'signup.controller.ts',
-});
 
 const signupController = async (req: Request, res: Response) => {
   const { email, password, name } = req.body;
@@ -17,8 +12,6 @@ const signupController = async (req: Request, res: Response) => {
       error: '중복된 항목이 있습니다.',
     });
   }
-
-  childLogger.info(registeredUser);
 
   return res.status(StatusCodes.CREATED).json('회원 가입이 정상적으로 되었습니다.');
 };
