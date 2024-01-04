d5414b9a36d8b82bf5f02464fa9e3edf09530358
jihwooon <jihwooon@gmail.com>
2024-01-04 22:01:18 +0900

    signin을 router에 추가하라

--- server/src/routers/users.router.ts
+++ server/src/routers/users.router.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import signupController from '../auth/signup/web/signup.controller';
+import signinController from '../auth/signin/web/signin.controller';
 
 const router = express.Router();
 
 router.post('/signup', signupController);
+router.post('/signin', signinController);
 
 export default router;
