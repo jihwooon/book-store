3a9e42f6625087e08c29a72205a918c914a14015
jihwooon <jihwooon@gmail.com>
2024-01-05 13:54:25 +0900

    signup controller에 관심사 분리를 하라
    signup controller에서 데이터베이스 저장하는 로직과 http 요청 처리 하는
    로직이 동시에 이뤄지고 있습니다.
    
    관심사를 분리하기 위해서 service 레이어를 나나누고 데이터베이스 저장
    로직을 서비스레이에 옮겼습니다.
    
    controller 테스트 코드를 수정하였습니다

--- server/src/auth/signup/web/signup.controller.test.ts
+++ server/src/auth/signup/web/signup.controller.test.ts
@@ -1,42 +1,39 @@
 import request from 'supertest';
 
 import app from '../../../app';
-import { save } from '../../domain/user.repository';
+import signupService from '../application/signup.service';
 
-jest.mock('../../domain/user.repository.ts');
+jest.mock('../application/signup.service.ts');
 
 describe('signup Controller', () => {
-  const email = 'abc@gmail.com';
-  const password = '12345';
+  const userMock = {
+    email: 'abc@gmail.com',
+    password: '1234',
+    name: '홍길동',
+  };
 
   describe('POST /signup', () => {
     context('회원 가입 정보가 주어지고 저장에 성공하면', () => {
       beforeEach(() => {
-        (save as jest.Mock).mockResolvedValue(() => true);
+        (signupService as jest.Mock).mockResolvedValue(true);
       });
       it('201 상태코드와 응답 메세지를 반환한다.', async () => {
-        const { statusCode, body } = await request(app).post('/signup').send({
-          email,
-          password,
-        });
+        const { statusCode, body: { messages: message } } = await request(app).post('/signup').send(userMock);
 
         expect(statusCode).toBe(201);
-        expect(body).toStrictEqual('회원 가입이 정상적으로 되었습니다.');
+        expect(message).toBe('회원 가입이 정상적으로 되었습니다.');
       });
     });
 
     context('회원 가입 정보가 주어지고 저장에 실패하면', () => {
       beforeEach(() => {
-        (save as jest.Mock).mockResolvedValue(() => false);
+        (signupService as jest.Mock).mockResolvedValue(false);
       });
       it('400 상태코드와 에러 메세지를 반환한다.', async () => {
-        const { statusCode, body } = await request(app).post('/signup').send({
-          email,
-          password,
-        });
+        const { statusCode, body: { messages } } = await request(app).post('/signup').send(userMock);
 
-        expect(statusCode).toBe(201);
-        expect(body).toStrictEqual('회원 가입이 정상적으로 되었습니다.');
+        expect(statusCode).toBe(400);
+        expect(messages).toBe('회원 가입에 실패했습니다.');
       });
     });
   });
--- server/src/auth/signup/web/signup.controller.ts
+++ server/src/auth/signup/web/signup.controller.ts
@@ -1,19 +1,21 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-import { save } from '../../domain/user.repository';
+import signupService from '../application/signup.service';
 
 const signupController = async (req: Request, res: Response) => {
   const { email, password, name } = req.body;
 
-  const registeredUser = await save(email, password, name);
+  const registeredUser = await signupService(email, password, name);
   if (!registeredUser) {
     return res.status(StatusCodes.BAD_REQUEST).json({
-      error: '중복된 항목이 있습니다.',
+      messages: '회원 가입에 실패했습니다.',
     });
   }
 
-  return res.status(StatusCodes.CREATED).json('회원 가입이 정상적으로 되었습니다.');
+  return res.status(StatusCodes.CREATED).json({
+    messages: '회원 가입이 정상적으로 되었습니다.',
+  });
 };
 
 export default signupController;
