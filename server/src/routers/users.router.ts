c4870ffe4f3147c9302665887d04a12b9f868759
jihwooon <jihwooon@gmail.com>
2024-01-05 09:35:18 +0900

    POST 요청을 처리하는 url reset을 router에 추가하라

--- server/src/routers/users.router.ts
+++ server/src/routers/users.router.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import signupController from '../auth/signup/web/signup.controller';
+import { passwordResetRequestController } from '../auth/reset/web/password-reset.controller';
 import signinController from '../auth/signin/web/signin.controller';
+import signupController from '../auth/signup/web/signup.controller';
 
 const router = express.Router();
 
 router.post('/signup', signupController);
 router.post('/signin', signinController);
+router.post('/reset', passwordResetRequestController);
 
 export default router;
