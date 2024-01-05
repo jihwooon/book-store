f64b65aaafa7383755ec1726b484393f9b2bf1aa
jihwooon <jihwooon@gmail.com>
2024-01-05 15:09:56 +0900

    signup service에 암호화 crypto를 추가하라
    패스워드를 salt 암호화 하는 crypto를 추가했습니다.
    암호화 된 비밀번호화 salt를 두 둘 저장하는 것을 수행합니다.

--- server/src/auth/signup/application/signup.service.ts
+++ server/src/auth/signup/application/signup.service.ts
@@ -1,7 +1,19 @@
+import crypto from 'crypto';
+
 import { save } from '../../domain/user.repository';
 
+const createHash = (password: string, salt: string) => {
+  const hashPassword = crypto.pbkdf2Sync(password, salt, 10000, 10, 'sha512').toString('base64');
+  return hashPassword;
+};
+
+const createSalt = () => crypto.randomBytes(10).toString('base64');
+
 const signupService = async (email: string, password: string, name: string): Promise<boolean> => {
-  const savedUser = await save(email, password, name);
+  const salt = createSalt();
+  const hashPassword = createHash(password, salt);
+
+  const savedUser = await save(email, hashPassword, name, salt);
   if (!savedUser) {
     throw new Error('중복된 항목이 있습니다.');
   }
