c55e3d9e3e967f525df529962502ca617ab6df15
jihwooon <jihwooon@gmail.com>
2024-01-05 09:44:40 +0900

    PUT 요청을 처리하는 url reset을 router에 추가하라

--- server/src/routers/users.router.ts
+++ server/src/routers/users.router.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { passwordResetRequestController } from '../auth/reset/web/password-reset.controller';
+import { passwordResetRequestController, passwordResetController } from '../auth/reset/web/password-reset.controller';
 import signinController from '../auth/signin/web/signin.controller';
 import signupController from '../auth/signup/web/signup.controller';
 
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post('/signup', signupController);
 router.post('/signin', signinController);
 router.post('/reset', passwordResetRequestController);
+router.put('/reset', passwordResetController);
 
 export default router;
