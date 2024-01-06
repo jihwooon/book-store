e54dc1b65d4db9e21bba14630b13112632b00995
jihwooon <jihwooon@gmail.com>
2024-01-06 13:43:54 +0900

    user 클래스 구현과 User 도메인 객체 인스터스화 하라
    관련 속성과 생서자 사용하여 User 클래스를 만들었습니다. 또한 사용자 도메인 개체를 인스턴스화했습니다.

---
+++ server/src/auth/domain/user.test.ts
@@ -0,0 +1,35 @@
+import User from './user';
+
+describe('User class', () => {
+  const userMock = {
+    id: 1,
+    email: 'abc@gmail.com',
+    password: '12345',
+    name: '홍길동',
+    salt: 'ARNhEBG6dKaNBg==',
+  };
+
+  context('객체에 값이 주어지면', () => {
+    const user = new User(userMock);
+
+    it('멤버 변수를 값을 리턴해야 한다.', () => {
+      expect(user.getId()).toBe(1);
+      expect(user.getEmail()).toBe('abc@gmail.com');
+      expect(user.getPassword()).toBe('12345');
+      expect(user.getSalt()).toBe('ARNhEBG6dKaNBg==');
+      expect(user.getName()).toBe('홍길동');
+    });
+  });
+
+  context('객체의 값이 주어지지 않으면', () => {
+    const user = new User({});
+
+    it('멤버 변수를 default 값을 리턴해야 한다.', () => {
+      expect(user.getId()).toBe(0);
+      expect(user.getEmail()).toBe('');
+      expect(user.getPassword()).toBe('');
+      expect(user.getSalt()).toBe('');
+      expect(user.getName()).toBe('');
+    });
+  });
+});
