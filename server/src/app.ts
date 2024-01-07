262a447668dc254912b6ca5e08b57180c9d6d2f8
jihwooon <jihwooon@gmail.com>
2024-01-07 17:56:32 +0900

    도서 관련 HTTP 요청 및 응답을 처리하기 위해 BookRouter를 구현합니다.
    도서 관련 HTTP 요청에 대한 라우팅을 처리하기 위해 새로운 `BookRouter`를 생성했습니다.

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import rTracer from 'cls-rtracer';
 
 import userRouter from './routers/users.router';
+import bookRouter from './routers/books.router';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(rTracer.expressMiddleware());
 
 app.use(userRouter);
+app.use(bookRouter);
 
 app.get('/', async (req, res) => {
   res.send('Hello World');
