99fd5ce1a3d54f67f0fd6aed88882d81002b73e4
jihwooon <jihwooon@gmail.com>
2024-01-09 01:55:18 +0900

    HTTP 응답 처리하는 ResponseHandler 함수를 정의하라
    URL 쿼리 스트링에 따라 요청을 분기 처리합니다. 이때 에러를 정의하기
    어려운 문제가 생겨 공통적으로 에러 처리를 하는 함수를 정의하고
    
    에러요청에 따라 에러 처리도 다르게 하도록 처리하였습니다

---
+++ server/src/utils/responseHandler.ts
@@ -0,0 +1,22 @@
+import { Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
+import logger from '../config/logger';
+
+export const ResponseHandler = async <T>(
+  func: () => Promise<T>, status: StatusCodes, res: Response,
+) => {
+  try {
+    const result = await func();
+
+    res.status(status).json({
+      data: result,
+    });
+  } catch (error: any) {
+    logger.error(error.message);
+
+    res.status(error.status).json({
+      message: error.message,
+    });
+  }
+};
