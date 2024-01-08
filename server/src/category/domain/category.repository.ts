7f4ebfee0ec995fdb8159919740a00244efe5abc
jihwooon <jihwooon@gmail.com>
2024-01-08 13:28:13 +0900

    categoryRepository에 findAll 함수를 생성하여 카테고리 전체 조회를 하라
    categoryRepository에 findAll 함수를 추가했습니다.

---
+++ server/src/category/domain/category.repository.ts
@@ -0,0 +1,15 @@
+import { RowDataPacket } from 'mysql2';
+
+import { doQuery } from '../../database/mariadb';
+import Category from './category';
+
+export const findAll = async (): Promise<Category[]> => {
+  const [rows] = await doQuery((connection) => connection.execute<RowDataPacket[]>(
+    'SELECT id, name FROM category',
+  ));
+
+  return (rows ?? []).map((row) => new Category({
+    id: row.id,
+    name: row.name,
+  }));
+};
