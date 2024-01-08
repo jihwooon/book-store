8fa2dcf0ab7466ffb0bd3fa3ff0666b8fb4322eb
jihwooon <jihwooon@gmail.com>
2024-01-09 02:49:06 +0900

    회원 가입 에러 핸들러와 응답 핸들러를 적용하라

--- server/src/users/signup/web/signup.controller.test.ts
+++ server/src/users/signup/web/signup.controller.test.ts
@@ -1,8 +1,11 @@
 import request from 'supertest';
 
+import { StatusCodes } from 'http-status-codes';
+
 import app from '../../../app';
+import { inValidUser, validUser } from '../../../fixture/user.fixture';
+import HttpException from '../../../utils/httpException';
 import signupService from '../application/signup.service';
-import { validUser, inValidUser } from '../../../fixture/user.fixture';
 
 jest.mock('../application/signup.service.ts');
 
@@ -12,23 +15,23 @@ describe('signup Controller', () => {
       beforeEach(() => {
         (signupService as jest.Mock).mockResolvedValue(true);
       });
-      it('201 상태코드와 응답 메세지를 반환한다.', async () => {
-        const { statusCode, body: { messages: message } } = await request(app).post('/signup').send(validUser);
+      it('201 상태코드를 반환한다.', async () => {
+        const { statusCode, body: { data } } = await request(app).post('/signup').send(validUser);
 
         expect(statusCode).toBe(201);
-        expect(message).toBe('회원 가입이 정상적으로 되었습니다.');
+        expect(data).toBe(true);
       });
     });
 
     context('사용자가 비정상 정보를 입력한 경우', () => {
       beforeEach(() => {
-        (signupService as jest.Mock).mockResolvedValue(false);
+        (signupService as jest.Mock).mockRejectedValue(new HttpException('회원 가입에 실패했습니다.', StatusCodes.BAD_REQUEST));
       });
       it('400 상태코드와 에러 메세지를 반환한다.', async () => {
-        const { statusCode, body: { messages } } = await request(app).post('/signup').send(inValidUser);
+        const { statusCode, body: { message } } = await request(app).post('/signup').send(inValidUser);
 
         expect(statusCode).toBe(400);
-        expect(messages).toBe('회원 가입에 실패했습니다.');
+        expect(message).toBe('회원 가입에 실패했습니다.');
       });
     });
   });
--- server/src/users/signup/web/signup.controller.ts
+++ server/src/users/signup/web/signup.controller.ts
@@ -1,21 +1,13 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import { ResponseHandler } from '../../../utils/responseHandler';
 import signupService from '../application/signup.service';
 
 const signupController = async (req: Request, res: Response) => {
   const { email, password, name } = req.body;
 
-  const registeredUser = await signupService(email, password, name);
-  if (!registeredUser) {
-    return res.status(StatusCodes.BAD_REQUEST).json({
-      messages: '회원 가입에 실패했습니다.',
-    });
-  }
-
-  return res.status(StatusCodes.CREATED).json({
-    messages: '회원 가입이 정상적으로 되었습니다.',
-  });
+  ResponseHandler(() => signupService(email, password, name), StatusCodes.CREATED, res);
 };
 
 export default signupController;
