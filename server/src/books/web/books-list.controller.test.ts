a978c24463f6a9fa633ea54743744001c9f8518c
jihwooon <jihwooon@gmail.com>
2024-01-09 20:22:40 +0900

    신간 목록 조회 컨트롤러를 작성하라
    신간 목록 조회 컨트롤러를 작성했습니다

--- server/src/books/web/books-list.controller.test.ts
+++ server/src/books/web/books-list.controller.test.ts
@@ -1,13 +1,21 @@
 /* eslint-disable jest/no-identical-title */
 import request from 'supertest';
 
+import { StatusCodes } from 'http-status-codes';
+
 import app from '../../app';
-import { existingBook, existingBooks } from '../../fixture/books.fixture';
-import getAllBooks from '../application/books-list.service';
+import {
+  existingBook, existingBooks, newReleaseBook,
+  notNewReleaseBook,
+} from '../../fixture/books.fixture';
+import HttpException from '../../utils/httpException';
 import getBooksByCategory from '../application/books-category.service';
+import getAllBooks from '../application/books-list.service';
+import { getBooksByNewRelease } from '../application/books-new-release.service';
 
 jest.mock('../application/books-list.service.ts');
 jest.mock('../application/books-category.service.ts');
+jest.mock('../application/books-new-release.service.ts');
 
 describe('bookList Controller', () => {
   context('GET /books', () => {
@@ -34,6 +42,61 @@ describe('bookList Controller', () => {
       expect(statusCode).toBe(200);
       expect(body).toEqual({
         data: existingBook,
+      });
+    });
+  });
+
+  describe('GET /books?category_id&&news', () => {
+    context('사용자가 카테고리 id와 신간 조회한 경우', () => {
+      beforeEach(() => {
+        (getBooksByNewRelease as jest.Mock).mockResolvedValue(newReleaseBook);
+      });
+      it('200 상태코드를 반환한다.', async () => {
+        const { statusCode, body } = await request(app).get('/books?category_id&&news')
+          .query({ category_id: '1', news: true });
+
+        expect(statusCode).toBe(200);
+        expect(body).toEqual({
+          data: newReleaseBook,
+        });
+      });
+    });
+
+    context('사용자가 신간 조회를 비정상적으로 입력된 경우', () => {
+      beforeEach(() => {
+        (getBooksByNewRelease as jest.Mock)
+          .mockRejectedValue(new HttpException('신간 도서가 아닙니다.', StatusCodes.BAD_REQUEST));
+      });
+      it('400 상태코드를 반환한다.', async () => {
+        const { statusCode, body } = await request(app).get('/books?category_id&&news')
+          .query({ category_id: '1', news: false });
+
+        expect(statusCode).toBe(400);
+        expect(body).toEqual({
+          message: '신간 도서가 아닙니다.',
+          status: 400,
+          timestamp: expect.any(String),
+        });
+      });
+    });
+
+    context('사용자가 비정상적으로 카테고리 정보가 입력된 경우', () => {
+      beforeEach(() => {
+        (getBooksByNewRelease as jest.Mock)
+          .mockRejectedValue(
+            new HttpException(`${notNewReleaseBook.categoryId} 카테고리 정보를 찾을 수 없습니다.`, StatusCodes.NOT_FOUND),
+          );
+      });
+      it('404 상태코드를 반환한다.', async () => {
+        const { statusCode, body } = await request(app).get('/books?category_id&&news')
+          .query({ category_id: notNewReleaseBook.categoryId, news: true });
+
+        expect(statusCode).toBe(404);
+        expect(body).toEqual({
+          message: `${notNewReleaseBook.categoryId} 카테고리 정보를 찾을 수 없습니다.`,
+          status: 404,
+          timestamp: expect.any(String),
+        });
       });
     });
   });
--- server/src/books/web/books-list.controller.ts
+++ server/src/books/web/books-list.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 /* eslint-disable camelcase */
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
@@ -5,13 +6,23 @@ import { StatusCodes } from 'http-status-codes';
 import { ResponseHandler } from '../../utils/responseHandler';
 import getBooksByCategory from '../application/books-category.service';
 import getAllBooks from '../application/books-list.service';
+import { getBooksByNewRelease } from '../application/books-new-release.service';
 
-const getAllBooksHandler = async ({ query: { category_id } }: Request, res: Response) => {
+const getAllBooksHandler = async ({ query: { category_id, news } }: Request, res: Response) => {
   if (!category_id) {
-    ResponseHandler(getAllBooks, StatusCodes.OK, res);
-  } else {
-    ResponseHandler(() => getBooksByCategory(Number(category_id)), StatusCodes.OK, res);
+    return ResponseHandler(getAllBooks, StatusCodes.OK, res);
+  } if (category_id && news) {
+    return ResponseHandler(() => getBooksByNewRelease(
+      Number(category_id),
+      Boolean(news),
+    ), StatusCodes.OK, res);
+  } if (news) {
+    return res.status(StatusCodes.OK).json({
+      message: 'Not yet implement',
+    });
   }
+
+  return ResponseHandler(() => getBooksByCategory(Number(category_id)), StatusCodes.OK, res);
 };
 
 export default getAllBooksHandler;
