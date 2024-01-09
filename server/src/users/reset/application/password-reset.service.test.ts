2eaf479405ab4033fc289389f928cd05d10f497c
jihwooon <jihwooon@gmail.com>
2024-01-09 11:36:58 +0900

    패스워드 초기화 서비스 로직으로 분리하라
    기존 파일에서 두 가지 함수를 하나의 함수로 분리했습니다

--- server/src/users/reset/application/password-reset.service.test.ts
+++ server/src/users/reset/application/password-reset.service.test.ts
@@ -1,35 +1,15 @@
-import { existingUser, inValidUser, validUser } from '../../../fixture/user.fixture';
-import User from '../../domain/user';
-import { findByEmail, updateUserByPasswordAndSalt } from '../../domain/user.repository';
-import { passwordResetRequester, passwordResetter } from './password-reset.service';
+import { StatusCodes } from 'http-status-codes';
+
+import { existingUser, validUser } from '../../../fixture/user.fixture';
+import HttpException from '../../../utils/httpException';
+import { updateUserByPasswordAndSalt } from '../../domain/user.repository';
+import passwordResetter from './password-reset.service';
 
 jest.mock('../../domain/user.repository.ts');
 
-describe('PasswordReset service', () => {
-  describe('passwordResetRequester', () => {
-    beforeEach(() => {
-      (findByEmail as jest.Mock).mockResolvedValue(new User(existingUser));
-    });
-    context('사용자의 email이 올바르게 입력된 경우', () => {
-      it('검증 된 이메일를 반환한다.', async () => {
-        const { email } = await passwordResetRequester(validUser.email);
-
-        expect(email).toBe('abc@gmail.com');
-      });
-    });
-
-    context('사용자의 email이 올바르지 않으면', () => {
-      beforeEach(() => {
-        (findByEmail as jest.Mock).mockResolvedValue(undefined);
-      });
-      it('Error을 던져야 한다', async () => {
-        await expect(passwordResetRequester(inValidUser.email)).rejects.toThrow(new Error('이메일을 찾을 수가 없습니다.'));
-      });
-    });
-  });
-
+describe('passwordResetRequester service', () => {
   describe('passwordResetter', () => {
-    context('사용자의 email과 password가 올바르게 입력되고 ', () => {
+    context('사용자의 email과 password가 올바르게 입력되면 ', () => {
       beforeEach(() => {
         (updateUserByPasswordAndSalt as jest.Mock).mockResolvedValue(true);
       });
@@ -40,14 +20,13 @@ describe('PasswordReset service', () => {
       });
     });
 
-    context('사용자의 email과 password가 비정상적으로 입력되고', () => {
+    context('사용자의 email과 password가 비정상적으로 입력되면', () => {
       beforeEach(() => {
         (updateUserByPasswordAndSalt as jest.Mock).mockResolvedValue(false);
       });
-      it('password 변경이 실패 되어야 한다.', async () => {
-        const failedChange = await passwordResetter(existingUser.email, existingUser.password);
-
-        expect(failedChange).toBe(false);
+      it('HttpException 에러를 던져야한다.', async () => {
+        await expect(passwordResetter(existingUser.email, existingUser.password))
+          .rejects.toThrow(new HttpException('패스워드 초기화에 실패했습니다.', StatusCodes.BAD_REQUEST));
       });
     });
   });
