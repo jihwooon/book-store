4c3be682858f325091dc4b13b307e05d9d833fb3
jihwooon <jihwooon@gmail.com>
2024-01-09 23:04:34 +0900

    신간 유무에 따라 신간 목록 조회 컨트롤러 로직을 작성하라

--- server/src/books/web/books-list.controller.ts
+++ server/src/books/web/books-list.controller.ts
@@ -5,24 +5,28 @@ import { StatusCodes } from 'http-status-codes';
 
 import { ResponseHandler } from '../../utils/responseHandler';
 import { getBooksByCategoryAndNewRelease } from '../application/books-category-new-release.service';
+import { getAllBooksByNewRelease } from '../application/books-new-release.service';
+
 import getBooksByCategory from '../application/books-category.service';
 import getAllBooks from '../application/books-list.service';
 
 const getAllBooksHandler = async ({ query: { category_id, news } }: Request, res: Response) => {
-  if (!category_id) {
-    return ResponseHandler(getAllBooks, StatusCodes.OK, res);
-  } if (category_id && news) {
+  if (category_id && news) {
     return ResponseHandler(() => getBooksByCategoryAndNewRelease(
       Number(category_id),
       Boolean(news),
     ), StatusCodes.OK, res);
-  } if (news) {
-    return res.status(StatusCodes.OK).json({
-      message: 'Not yet implement',
-    });
   }
 
-  return ResponseHandler(() => getBooksByCategory(Number(category_id)), StatusCodes.OK, res);
+  if (category_id) {
+    return ResponseHandler(() => getBooksByCategory(Number(category_id)), StatusCodes.OK, res);
+  }
+
+  if (news) {
+    return ResponseHandler(() => getAllBooksByNewRelease(Boolean(news)), StatusCodes.OK, res);
+  }
+
+  return ResponseHandler(getAllBooks, StatusCodes.OK, res);
 };
 
 export default getAllBooksHandler;
