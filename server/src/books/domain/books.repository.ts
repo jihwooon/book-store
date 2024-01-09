0db653282c77ea0f295eac2116c4c6f223232184
jihwooon <jihwooon@gmail.com>
2024-01-09 20:52:26 +0900

    BookRepository에 신간 조회 함수를 추가하라

--- server/src/books/domain/books.repository.ts
+++ server/src/books/domain/books.repository.ts
@@ -116,3 +116,35 @@ export const findByCategoryAndNewRelease = async (
     pubDate: row.pub_date,
   });
 };
+
+export const findByNewRelease = async (
+  categoryId: number,
+): Promise<Book> => {
+  const [rows] = await doQuery((connection) => connection.execute<RowDataPacket[]>(
+    'SELECT * FROM books WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()'
+    , [categoryId],
+  ));
+
+  const [row] = rows ?? [];
+  if (!row) {
+    childLogger.error(row);
+    return undefined;
+  }
+
+  return new Book({
+    id: row.id,
+    title: row.title,
+    imgId: row.img_id,
+    categoryId: row.category_id,
+    form: row.form,
+    isbn: row.isbn,
+    summary: row.summary,
+    detail: row.detail,
+    author: row.author,
+    pages: row.pages,
+    contents: row.contents,
+    price: row.price,
+    likes: row.likes,
+    pubDate: row.pub_date,
+  });
+};
