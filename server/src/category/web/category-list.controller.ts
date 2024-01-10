b7c60f304ef92ba180ce599f78e12eac2f43ee30
jihwooon <jihwooon@gmail.com>
2024-01-10 13:20:59 +0900

    Category 패키지 절대 경로를 상대 경로로 수정하라

--- server/src/category/web/category-list.controller.ts
+++ server/src/category/web/category-list.controller.ts
@@ -2,7 +2,8 @@ import { Request, Response } from 'express';
 
 import { StatusCodes } from 'http-status-codes';
 
-import { ResponseHandler } from '../../utils/responseHandler';
+import { ResponseHandler } from 'src/utils/responseHandler';
+
 import getAllCategory from '../application/category-list.service';
 
 const getAllCategoryHandler = async (req: Request, res: Response) => {
