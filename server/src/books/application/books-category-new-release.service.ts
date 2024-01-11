dc619630ec83e4939f523a3d7a93a9dd686bdda4
jihwooon <jihwooon@gmail.com>
2024-01-12 00:20:36 +0900

    카테고리별 신간 조회 BookList Service에 Limit와 Offset를 추가하라

--- server/src/books/application/books-category-new-release.service.ts
+++ server/src/books/application/books-category-new-release.service.ts
@@ -4,13 +4,20 @@ import { StatusCodes } from 'http-status-codes';
 
 import { findByCategoryAndNewRelease } from '../domain/books.repository';
 
-export const getBooksByCategoryAndNewRelease = async (categoryId: number) => {
-  const newReleaseBooks = await findByCategoryAndNewRelease(categoryId);
+export const getBooksByCategoryAndNewRelease = async (
+  categoryId: number,
+  limit: number,
+  currentPage: number,
+) => {
+  const { books, totalCount } = await findByCategoryAndNewRelease(categoryId, limit, currentPage);
 
-  const isCategory = newReleaseBooks.some((book) => book.getCategoryId() === categoryId);
+  const isCategory = books.some((book) => book.getCategoryId() === categoryId);
   if (!isCategory) {
     throw new HttpException(`${categoryId} 카테고리 정보를 찾을 수 없습니다.`, StatusCodes.NOT_FOUND);
   }
 
-  return newReleaseBooks;
+  return {
+    books,
+    totalCount,
+  };
 };
