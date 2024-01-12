6674cdbafa58ddb93680661525327b2719e69612
jihwooon <jihwooon@gmail.com>
2024-01-13 00:59:55 +0900

    유효성 검사하는 valiationHandler를 추가하라

---
+++ server/src/utils/valiationHandler.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject, ZodError } from 'zod';
+
+export const validateHandler = <T extends AnyZodObject>(schema: T) => async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+// eslint-disable-next-line consistent-return
+) => {
+  try {
+    await schema.parseAsync({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    });
+    return next();
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json(error);
+    }
+  }
+};
