8e123ed4e602369a95a8867634cb262a5b246c1f
jihwooon <jihwooon@gmail.com>
2024-01-12 16:13:30 +0900

    카테고리별 신간 조회 BookList Service에 Limit와 Offset을 추가하라

--- server/src/books/application/books-category.service.ts
+++ server/src/books/application/books-category.service.ts
@@ -5,13 +5,20 @@ import HttpException from 'src/utils/httpException';
 import Book from '../domain/book';
 import { findByCategory } from '../domain/books.repository';
 
-const getBooksByCategory = async (categoryId: number): Promise<Book> => {
-  const categories = await findByCategory(categoryId);
-  if (!categories) {
+const getBooksByCategory = async (
+  categoryId: number,
+  limit: number,
+  currentPage: number,
+): Promise<{ books: Book[], totalCount: number }> => {
+  const { books, totalCount } = await findByCategory(categoryId, limit, currentPage);
+  if (books.length === 0) {
     throw new HttpException(`해당 ${categoryId}를 찾을 수 없습니다.`, StatusCodes.NOT_FOUND);
   }
 
-  return categories;
+  return {
+    books,
+    totalCount,
+  };
 };
 
 export default getBooksByCategory;
