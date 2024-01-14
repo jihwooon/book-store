a1e45bd93e7e9d63dbbdd5c05b3632f4168ecf01
jihwooon <jihwooon@gmail.com>
2024-01-14 14:11:28 +0900

    BookRepository에 findWithCategory 함수에 like 갯수 subQuery를 추가하라

--- server/src/books/domain/books.repository.ts
+++ server/src/books/domain/books.repository.ts
@@ -44,14 +44,16 @@ export const findAll = async (limit: number, currentPage: number): Promise<{
   };
 };
 
-export const findWithCategory = async (id: number): Promise<Book> => {
+export const findWithCategory = async (bookId: number, userId: number): Promise<Book> => {
   const [rows] = await doQuery((connection) => connection.execute<RowDataPacket[]>(
-    `SELECT b.id, b.title, b.category_id, b.form, b.isbn, b.summary, b.detail, b.author, b.pages, b.contents, b.price, b.likes, b.pub_date
+    `SELECT b.id, b.title, b.category_id, b.form, b.isbn, b.summary, b.detail, b.author, b.pages, b.contents, b.price, b.pub_date,
+            (SELECT count(*) FROM likes WHERE b.id = liked_book_id) as likes,
+            (SELECT EXISTS(SELECT * FROM likes WHERE user_id = ? AND liked_book_id)) as liked
        FROM books b
        LEFT JOIN category c
          ON b.category_id = c.id
       WHERE b.id = ?`,
-    [id],
+    [userId, bookId],
   ));
 
   const [row] = rows ?? [];
