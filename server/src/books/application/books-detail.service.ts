8f21562fa627b988ffb2048dbc3cb71be911c906
jihwooon <jihwooon@gmail.com>
2024-01-14 14:12:33 +0900

    BookDetail Service에 userId를 추가하라

--- server/src/books/application/books-detail.service.ts
+++ server/src/books/application/books-detail.service.ts
@@ -5,10 +5,10 @@ import HttpException from 'src/utils/httpException';
 import type Book from '../domain/book';
 import { findWithCategory } from '../domain/books.repository';
 
-const getDetailBook = async (id: number): Promise<Book> => {
-  const book = await findWithCategory(id);
+const getDetailBook = async (userId: number, bookId: number): Promise<Book> => {
+  const book = await findWithCategory(userId, bookId);
   if (!book) {
-    throw new HttpException(`${id} 해당하는 도서 정보를 찾을 수 없습니다.`, StatusCodes.NOT_FOUND);
+    throw new HttpException(`${bookId} 해당하는 도서 정보를 찾을 수 없습니다.`, StatusCodes.NOT_FOUND);
   }
 
   return book;
