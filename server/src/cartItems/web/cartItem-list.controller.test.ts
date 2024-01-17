ccfb9e619ddf0348abc2851ee3dfa62ad75e722c
jihwooon <jihwooon@gmail.com>
2024-01-17 13:26:49 +0900

    장바구니 도서 목록 Controller를 추가하라

---
+++ server/src/cartItems/web/cartItem-list.controller.test.ts
@@ -0,0 +1,52 @@
+import app from 'src/app';
+import { cartItemBookList, existingCartItem, nonExistingCartItem } from 'src/fixture/cartItem.fixture';
+
+import request from 'supertest';
+
+import { StatusCodes } from 'http-status-codes';
+import HttpException from 'src/utils/httpException';
+
+import { getCartItems } from '../application/cartItem-list.service';
+
+jest.mock('../application/cartItem-list.service.ts');
+
+describe('cartItemList Controller', () => {
+  beforeEach(() => {
+    (getCartItems as jest.Mock).mockResolvedValue(cartItemBookList);
+  });
+
+  describe('GET /cart', () => {
+    context('사용자가 장바구니에 도서와 수량을 추가하면', () => {
+      it('201 상태코드를 반환한다.', async () => {
+        const { status, body: { data } } = await request(app)
+          .get(`/cart/${existingCartItem.bookId}`);
+
+        expect(status).toBe(200);
+        expect(data).toEqual(cartItemBookList);
+      });
+    });
+
+    context('장바구니 내에 도서 정보 id가 올바르지 않는 경우', () => {
+      beforeEach(() => {
+        (getCartItems as jest.Mock).mockRejectedValue(
+          new HttpException(
+            '장바구니가 내 도서 정보가 존재하지 않습니다.',
+            StatusCodes.NOT_FOUND,
+          ),
+        );
+      });
+
+      it('404 상태코드와 에러 메세지를 반환한다.', async () => {
+        const { status, body } = await request(app)
+          .get(`/cart/${nonExistingCartItem.bookId}`);
+
+        expect(status).toBe(404);
+        expect(body).toEqual({
+          message: '장바구니가 내 도서 정보가 존재하지 않습니다.',
+          status: 404,
+          timestamp: expect.any(String),
+        });
+      });
+    });
+  });
+});
---
+++ server/src/cartItems/web/cartItem-list.controller.ts
@@ -0,0 +1,15 @@
+import { type Request, type Response } from 'express';
+
+import { StatusCodes } from 'http-status-codes';
+import { ResponseHandler } from 'src/utils/responseHandler';
+
+import { getCartItems } from '../application/cartItem-list.service';
+
+const getCartHandler = async (
+  { params: { bookId } }: Request,
+  res: Response,
+) => {
+  ResponseHandler(() => getCartItems(Number(bookId)), StatusCodes.OK, res);
+};
+
+export default getCartHandler;
