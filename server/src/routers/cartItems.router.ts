4ff38a053e5fe449e066d1f882d9f414da656118
jihwooon <jihwooon@gmail.com>
2024-01-17 21:59:18 +0900

    장바구니 삭제 Controller를 추가하라

--- server/src/routers/cartItems.router.ts
+++ server/src/routers/cartItems.router.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import getCartHandler from 'src/cartItems/web/cartItem-list.controller';
+import removeCartHandler from 'src/cartItems/web/cartItem-remove.controller';
 import addCartHandler from 'src/cartItems/web/cartItem-save.controller';
 
 const router = express.Router();
 
 router.post('/cart', addCartHandler);
 router.get('/cart/:bookId', getCartHandler);
+router.delete('/cart/:id', removeCartHandler);
 
 export default router;
