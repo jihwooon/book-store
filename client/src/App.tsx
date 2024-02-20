aec9afeee5f0b21e4d2fe5cec2ba672bbaaf436d
jihwooon <jihwooon@gmail.com>
2024-02-21 02:50:40 +0900

    프로젝트 구조 개선 및 스타일링 추가하라
    html 파일에 Pico.css 라이브러리를 추가하라
    App.tsx 파일에 Layout 컴포넌트를 사용하여 애플리케이션 레이아웃을
    정의하라

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
+import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div className='App'>
+    <Layout>
       <Home/>
-      </div>
+    </Layout>
   );
 }
 
---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <>
+        <hr/>
+        <footer>copyright(c), 2024, book store.</footer>
+      </>  
+    )
+}
+
+export default Footer;
