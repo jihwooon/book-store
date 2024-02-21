c00bab320678132ea30a76410fbfbdc9f56429c4
jihwooon <jihwooon@gmail.com>
2024-02-21 21:34:52 +0900

    스타일링 업데이트 (styled-components 추가)
    스타일링 라이브러리 styled-components를 패키지에 추가하여 전역 스타일 적용 및 Header 컴포넌트 스타일링을 구현했습니다.
    추가적으로 CSS 정리 라이브러리 sanitize.css도 설치하여 기본적인 스타일을 초기화했습니다.
    
    변경된 파일
    * client/package.json: styled-components 라이브러리 추가
    * client/src/components/common/Header.tsx: Header 컴포넌트 스타일링 추가
    * client/src/style/global.ts: 전역 스타일 정의
    
    **참고:**
    * styled-components 사용법에 대한 안내는 공식 문서를 참조하세요: https://styled-components.com/

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,9 +1,19 @@
+import styled from "styled-components";
+
 const Header = () => {
     return (
-        <header>
+        <HeaderStyle>
             <h1>book store</h1>
-        </header>
+        </HeaderStyle>
     )
 }
+
+const HeaderStyle = styled.header`
+  background-color: #333;
+
+  h1 {
+    color: white;
+  }
+`
 
 export default Header;
