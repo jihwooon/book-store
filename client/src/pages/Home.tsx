a0250d3836fc8bb10e6d604c07eaae0423df9c52
jihwooon <jihwooon@gmail.com>
2024-02-22 03:05:42 +0900

    Home 페이지에 타이틀 및 테마를 적용하라
    Home 페이지 Title 컴포넌트 사용하였습니다
    테마 스타일과 heading 스타일 (large, medium, small) 정의를 추가했습니다
    
    변경된 파일
    - Home.tsx
    - theme.ts

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import Title from "../components/common/Title";
+
 const Home = () => {
   return (
     <>
+      <Title size='medium' color='background'>제목 테스트</Title>
       <div>detail</div>
     </>
   )
--- client/src/style/theme.ts
+++ client/src/style/theme.ts
@@ -1,33 +1,52 @@
 export type ThemeName = "light" | "dark";
-type ColorKey = "primary" | "background" | "secondary" | "third";
+export type ColorKey = "primary" | "background" | "secondary" | "third";
 
 interface Theme {
-    name: ThemeName;
-    color: Record<ColorKey, string>
+  name: ThemeName;
+  color: Record<ColorKey, string>
+  heading: {
+    [key in HeadingSize]: {
+      fontSize: string;
+    }
+  };
 }
 
+export type HeadingSize = 'large' | 'medium' | 'small'
+
 export const light: Theme = {
-    name: 'light',
-    color: {
-      primary: 'brown',
-      background: 'lightgray',
-      secondary: 'blue',
-      third: 'green'
+  name: 'light',
+  color: {
+    primary: 'brown',
+    background: 'lightgray',
+    secondary: 'blue',
+    third: 'green'
+  },
+  heading: {
+    large: {
+      fontSize: '2rem'
+    },
+    medium: {
+      fontSize: '1.5rem'
+    },
+    small: {
+      fontSize: '1rem'
     }
+  }
 }
 
 export const dark: Theme = {
-    name: 'dark',
-    color: {
-      primary: 'coral',
-      background: 'midnightblue',
-      secondary: 'darkblue',
-      third: 'darkgreen'
-    }
+  ...light,
+  name: 'dark',
+  color: {
+    primary: 'coral',
+    background: 'midnightblue',
+    secondary: 'darkblue',
+    third: 'darkgreen'
+  },
 }
 
 export const getTheme = (themeName: ThemeName): Theme => {
-  switch(themeName) {
+  switch (themeName) {
     case 'light':
       return light
     case 'dark':
