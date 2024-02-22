d6aab6e2ade60144f698c8ef1c2628660a9f4c15
jihwooon <jihwooon@gmail.com>
2024-02-22 16:19:29 +0900

    헤더 간소화 및 로고 이미지 추가하라
    본 커밋은 다음과 같은 작업을 수행했습니다.
    
    - 테마 전환기를 헤더에서 제거하고 더욱 간결한 구조로 개선
    - 레이아웃 컴포넌트 내 Home 컴포넌트 직접 렌더링
    - 사이트 디자인 개선을 위해 로고 이미지 추가
    
    변경 사항
    - client/src/App.tsx: 헤더 간소화, Home 컴포넌트 직접 렌더링
    - client/src/assets/images/logo.png: 로고 이미지 추가
    
    변경된 파일
    - client/src/App.tsx
    - client/src/assets/images/logo.png (새로운 파일)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,13 @@
 import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import { BookStoreThemeProvider } from './context/themeContext';
-import ThemeSwitcher from './components/header/ThemeSwitcher';
 
 function App() {
   return (
     <BookStoreThemeProvider >
-      <ThemeSwitcher />
-        <Layout>
-          <Home/>
-        </Layout>
+      <Layout>
+        <Home/>
+      </Layout>
     </BookStoreThemeProvider>
   );
 }
