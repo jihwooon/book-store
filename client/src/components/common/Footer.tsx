f9029fef7e06fc91472c90127eeb42765564723a
jihwooon <jihwooon@gmail.com>
2024-02-22 16:16:21 +0900

    Footer 디자인 개선 및 로고 추가하라
    본 커밋은 다음과 같은 작업을 수행했습니다.
    
    - Footer 컴포넌트에 로고 이미지 추가
    - Footer 스타일을 변경하여 레이아웃과 일관성 유지
    - Copyright 문구는 기존 내용 그대로 유지
    
    변경 사항
    - Footer.tsx: 로고 이미지 추가 및 스타일 적용, FooterStyed 컴포넌트 사용, 레이아웃 스타일과 일치하도록 변경
    
    변경된 파일
    - client/src/components/common/Footer.tsx
    
    참고
    - 이 변경은 사이트의 전반적인 디자인 개선에 기여합니다.

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,10 +1,41 @@
+import styled from "styled-components";
+import logo from '../../assets/images/logo.png'
+
 const Footer = () => {
-    return (
-      <>
-        <hr/>
-        <footer>copyright(c), 2024, book store.</footer>
-      </>  
-    )
+  return (
+    <FooterStyed>
+      <h1 className="logo">
+        <img src={logo} alt="book store" />
+      </h1>
+      <div className="copyright">
+        <p>copyright(c), 2024, book store.</p>
+      </div>
+    </FooterStyed>  
+  )
 }
+
+const FooterStyed = styled.footer`
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${({ theme }) => theme.layout.width.large};
+  border-top: 1px solid ${({ theme }) => theme.color.background};
+  padding: 20px 0;
+  display: flex;
+  justify-content: space-between;
+  
+
+  .logo {
+    img {
+      width: 140px
+    }
+  }
+
+  .copyright {
+    p {
+      font-size: 0.75rem;
+      color: ${({ theme }) => theme.color.text};
+    }
+  }
+`
 
 export default Footer;
