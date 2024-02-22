f99f04adc475d385fe0c65bc4e2994930549778f
jihwooon <jihwooon@gmail.com>
2024-02-22 16:10:56 +0900

    Header 컴포넌트에 로고, 카테고리, 로그인/회원가입 기능 추가
    본 커밋은 다음과 같은 작업을 수행했습니다.
    
    - Header 컴포넌트에 logo, category, auth 섹션을 추가했습니다.
    - CATEGORY 배열을 정의하여 카테고리 링크를 생성했습니다.
    - react-icons를 사용하여 로그인/회원가입 아이콘을 추가했습니다.
    - HeaderStyle을 수정하여 레이아웃, 스타일을 조정했습니다.
    
    변경된 파일
    - client/src/components/common/Header.tsx
    
    참고
    - 카테고리 링크는 조건부 렌더링을 사용하여 전체 카테고리 링크를 포함했습니다.
    - 로그인/회원가입 링크는 현재 페이지에서 처리되지 않고 /login, /signup으로 연결됩니다.

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,18 +1,113 @@
 import styled from "styled-components";
+import { FaSignInAlt, FaRegUser } from "react-icons/fa";
+import logo from '../../assets/images/logo.png'
+
+const CATEGORY = [
+  {
+    id: null,
+    name: '전체'
+  },
+  {
+    id: 0,
+    name: '동화'
+  },
+  {
+    id: 1,
+    name: '소설'
+  },
+  {
+    id: 2,
+    name: '사회'
+  },
+]
 
 const Header = () => {
-    return (
-        <HeaderStyle>
-            <h1>book store</h1>
-        </HeaderStyle>
-    )
+  return (
+    <HeaderStyle>
+      <h1 className='logo'>
+        <img src={logo} alt='book store'></img>
+      </h1>
+      <nav className="category">
+        <ul>
+          {
+            CATEGORY.map((item) => (
+              <li key={item.id}>
+                <a href={item.id === null ? '/books' :`/books?category_id=${item.id}`}>        
+                  {item.name}
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+      </nav>
+      <nav className="auth">
+        <ul>
+          <li>
+            <a href="/login"><FaSignInAlt/>로그인</a>
+          </li>
+          <li>
+            <a href="/signup"><FaRegUser/> 회원가입</a>
+          </li>
+        </ul>
+      </nav>
+    </HeaderStyle>
+  )
 }
 
 const HeaderStyle = styled.header`
-  background-color: ${({ theme }) => theme.color.background};
+  width: 100%;
+  margin: 0 auto;
+  max-width: ${({ theme }) => theme.layout.width.large};
+  
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+  border-bottom: 1px solid ${({ theme }) => theme.color.background};
 
-  h1 {
-    color: ${({ theme }) => theme.color.primary};
+  .logo {
+    img {
+      width: 200px
+    }
+  }
+
+  .category {
+    ul {
+      display: flex;
+      gap: 32px;
+      li {
+        a {
+          font-size: 1.5rem;
+          font-weight: 600;
+          text-decoration: none;
+          color: ${({ theme }) => theme.color.text};
+
+          &:hover {
+            color: ${({ theme }) => theme.color.primary};
+          }
+        }
+      }
+    }
+  }
+
+  .auth {
+    ul {
+      display: flex;
+      gap: 16px;
+      li {
+        a {
+          font-size: 1rem;
+          font-weight: 600;
+          text-decoration: none;
+          display: flex;
+          align-items: center;
+          line-height: 1;
+
+          svg {
+            margin-right: 6px;
+          }
+        }
+      }
+    }
   }
 `
 
