897d79855f18025576af88d7991e515855580f47
jihwooon <jihwooon@gmail.com>
2024-03-10 10:30:07 +0900

    주문 서비스에 배송 기능 연동
    - 주문 서비스 (order-save.service.ts)에서 배송 서비스 (saveDelivery) 호출하여 배송 정보 저장
    - 컨트롤러 (orders-save.controller.ts)와 테스트 코드 (orders-save.controller.test.ts)도 수정

--- server/src/orders/web/orders-save.controller.ts
+++ server/src/orders/web/orders-save.controller.ts
@@ -1,9 +1,9 @@
 import { type Request, type Response } from 'express';
 
-import { saveOrders } from '../application/order-save.service';
+import { order } from '../application/order-save.service';
 
 const saveOrdersHandler = async ({ body: { items, delivery, totalPrice } }: Request, res: Response) => {
-  const savedOrder = await saveOrders(items, delivery, totalPrice);
+  const savedOrder = await order(items, delivery, totalPrice);
 
   return res.status(200).json(savedOrder);
 };
