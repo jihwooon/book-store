76e453ea57bf4f67f5e9945ab477f7f933339914
jihwooon <jihwooon@gmail.com>
2024-03-12 10:36:05 +0900

    주문 상세 조회 컨트롤러 및 서비스 추가
    주요 변경 사항
    - 주문 상세 정보 조회 기능을 위한 새로운 컨트롤러와 서비스를 추가했습니다.
      - server/src/orders/web/orders-detail.controller.ts (컨트롤러)
      - server/src/orders/application/orders-detail.service.ts (서비스, 아직 구현되지 않음)
    - 컨트롤러는 주문 상세 정보 조회 API를 제공하며, 서비스는 아직 구현되지 않았습니다.

--- server/src/orders/web/orders-detail.controller.ts
+++ server/src/orders/web/orders-detail.controller.ts
@@ -1,29 +1,12 @@
 import { type Request, type Response } from 'express';
 
+import { StatusCodes } from 'http-status-codes';
+import { ResponseHandler } from 'src/utils/responseHandler';
+
+import { getDetailOrders } from '../application/orders-detail.service';
+
 const getOrdersDetailHandler = async ({ params: { orderId } }: Request, res: Response) => {
-  return res.status(200).json([
-    {
-      bookId: 1,
-      bookTitle: '홍길동전',
-      author: '작가미상',
-      price: 8000,
-      count: 2,
-    },
-    {
-      bookId: 2,
-      bookTitle: '러닝 리액트',
-      author: '알렉스 뱅크스',
-      price: 25000,
-      count: 1,
-    },
-    {
-      bookId: 3,
-      bookTitle: '우아한 타입스크립트 with 리액트',
-      author: '우아한형제들 웹프론트그룹 지음',
-      price: 22500,
-      count: 10,
-    },
-  ]);
+  ResponseHandler(() => getDetailOrders(Number(orderId)), StatusCodes.OK, res);
 };
 
 export default getOrdersDetailHandler;
