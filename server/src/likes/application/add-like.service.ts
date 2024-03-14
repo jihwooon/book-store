0f6e1cd1c1e4719267a74bf9976e3b69060d2f5b
jihwooon <jihwooon@gmail.com>
2024-03-14 16:36:09 +0900

    좋아요 기능 - JWT 인증 및 개선
    - 좋아요 기능을 사용자 인증과 연동했습니다.
    - addLike 서비스에서 사용자 ID 를 받는 방식을 엑세스 토큰으로 변경했습니다.
    - users/jwt/jwt.provider.ts 의 validateToken 함수를 사용하여 엑세스 토큰을 검증하고 사용자 ID 를 추출합니다.
    - add-like.controller.ts 에서 요청 헤더에서 엑세스 토큰을 가져오도록 수정했습니다.

--- server/src/likes/application/add-like.service.ts
+++ server/src/likes/application/add-like.service.ts
@@ -1,10 +1,13 @@
 import { StatusCodes } from 'http-status-codes';
 import HttpException from 'src/utils/httpException';
 
+import { validateToken } from 'src/users/jwt/jwt.provider';
+
 import Like from '../domain/like';
 import { save } from '../domain/likes.repository';
 
-export const addLike = async (userId: number, likedBookId: number): Promise<boolean> => {
+export const addLike = async (accessToken: any, likedBookId: number): Promise<boolean> => {
+  const { userId } = validateToken(accessToken);
   const like = new Like({ userId, likedBookId });
   const likeData = like.getDataOfLike();
 
