bed5ea7c2a53a29eaf476b2113800bcbab667cc0
jihwooon <jihwooon@gmail.com>
2024-03-16 13:34:33 +0900

    주문 목록 조회 에러 처리 및 테스트 코드 추가
    - 주문 목록 조회 컨트롤러 (server/src/orders/web/orders-list.controller.ts)에 에러 처리 로직을 추가했습니다.
      - 주문 목록 조회에 실패하면 NOT_FOUND 에러를 발생시킵니다.
    - 테스트 코드 (server/src/orders/web/orders-list.controller.test.ts)를 업데이트하여 추가된 에러 처리 로직을 검증합니다.
      - 모의 서비스 에러를 발생시켜 컨트롤러가 예외를 처리하는지 테스트합니다.

--- server/src/orders/web/orders-list.controller.test.ts
+++ server/src/orders/web/orders-list.controller.test.ts
@@ -1,9 +1,39 @@
 import app from 'src/app';
 import request from 'supertest';
 
+import { StatusCodes } from 'http-status-codes';
+import HttpException from 'src/utils/httpException';
+
+import { getAllOrders } from '../application/orders-list.service';
+
+jest.mock('../application/orders-list.service.ts');
+
 describe('getAllOrdersHandler Controller', () => {
   describe('GET /orders', () => {
     context('주문 조회에 성공하면', () => {
+      beforeEach(() => {
+        (getAllOrders as jest.Mock).mockReturnValue([
+          {
+            address: '강원도 춘천시 동내면 대룡산길 227-314 24408 한국',
+            bookTitle: '어린왕자',
+            contact: '010-1234-5667',
+            orderId: 1,
+            receiver: '홍길동',
+            totalPrice: 6000,
+            totalQuantity: 3,
+          },
+          {
+            address: '강원도 춘천시 동내면 대룡산길 227-314 24408 한국',
+            bookTitle: '어린왕자',
+            contact: '010-1234-5667',
+            orderId: 2,
+            receiver: '홍길동',
+            totalPrice: 6000,
+            totalQuantity: 3,
+          },
+        ]);
+      });
+
       it('200 상태코드와 응답 메세지를 반환한다.', async () => {
         const { status, body } = await request(app).get('/orders');
 
@@ -28,6 +58,26 @@ describe('getAllOrdersHandler Controller', () => {
             totalQuantity: 3,
           },
         ]);
+      });
+    });
+
+    context('주문 조회 요청 실패 시', () => {
+      beforeEach(() => {
+        (getAllOrders as jest.Mock).mockRejectedValue(
+          new HttpException('주문 목록을 찾을 수 없습니다.', StatusCodes.NOT_FOUND),
+        );
+      });
+
+      it('404 상태코드와 에러 메세지를 반환한다', async () => {
+        const { status, body } = await request(app).get('/orders');
+
+        expect(status).toBe(404);
+        expect(body).toEqual({
+          message: '주문 목록을 찾을 수 없습니다.',
+          status: 404,
+          success: false,
+          timestamp: expect.any(String),
+        });
       });
     });
   });
